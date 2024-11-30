f6c2d97b9a71aef26e3f4cd196aeaea9d4f937f2
Azukuni <63687010+Asriel115@users.noreply.github.com>
2024-11-29 20:53:59 -0500

    Add files via upload
    more progress UwU

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <pwd.h>
+#include <fstream>
 
 namespace fs = std::filesystem;
 static const std::string characters{"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:',.<>?/"};
@@ -20,15 +21,22 @@ fs::path get_home_directory() {
         throw std::runtime_error("Could not determine the home directory.");
     }
 }
-
+    std::string password{};
 void pwdGen() {
 
+    for(int i = 0; i < 40; i++){
+        password += characters[std::rand() % characters.size()+0];
+    }
 }
 
 int main() {
     try {
-        fs::path homeDir = get_home_directory();
+        fs::path homeDir = get_home_directory() / std::string ("password.txt");
+        std::ofstream pwdOutput(homeDir);
         std::cout << "Home Directory: " << homeDir << "\n";
+        pwdGen();
+        pwdOutput << password << std::endl;
+        pwdOutput.close();
     } catch (const std::exception& e) {
         std::cerr << e.what() << "\n";
     }
